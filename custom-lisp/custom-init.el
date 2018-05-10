c5d238fc0c0e3568272b66fd55b06af597261f0c
wpzero <wpcreep@gmail.com>
2018-05-10 22:38:00 +0800

    Add-open-file-by-external-app-on-dired

--- custom-lisp/custom-init.el
+++ custom-lisp/custom-init.el
@@ -58,6 +58,37 @@
         (kill-region (region-beginning) (region-end))))
      (t (delete-backward-char 1)))))
 
+;;; open external app on dired
+(defun open-in-external-app ()
+  "Open the current file or dired marked files in external app.
+The app is chosen from your OS's preference.
+URL `http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html'
+Version 2016-10-15"
+  (interactive)
+  (let* (
+         ($file-list
+          (if (string-equal major-mode "dired-mode")
+              (dired-get-marked-files)
+            (list (buffer-file-name))))
+         ($do-it-p (if (<= (length $file-list) 5)
+                       t
+                     (y-or-n-p "Open more than 5 files? "))))
+    (when $do-it-p
+      (cond
+       ((string-equal system-type "windows-nt")
+        (mapc
+         (lambda ($fpath)
+           (w32-shell-execute "open" (replace-regexp-in-string "/" "\\" $fpath t t))) $file-list))
+       ((string-equal system-type "darwin")
+        (mapc
+         (lambda ($fpath)
+           (shell-command
+            (concat "open " (shell-quote-argument $fpath))))  $file-list))
+       ((string-equal system-type "gnu/linux")
+        (mapc
+         (lambda ($fpath) (let ((process-connection-type nil))
+                       (start-process "" nil "xdg-open" $fpath))) $file-list))))))
+
 ;;; paste or paste previouse
 ;; (defun past-or-past-previouse ()
 ;;   "Paste. When called repeatedly, paste previous. This command calls `yank', and if repeated, call `yank-pop'."
