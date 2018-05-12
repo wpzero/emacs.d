71b52714df248daa48df833c9c350c3b63ed786b
wpzero <wpcreep@gmail.com>
2018-05-12 08:19:25 +0800

    extract-c-config-and-add-multi-term-and-add-open-extern-use-dired

--- custom-lisp/custom-init.el
+++ custom-lisp/custom-init.el
@@ -5,10 +5,10 @@
 ;; (global-set-key (kbd "C-c C-n") 'mc/mark-next-like-this)
 ;; (global-set-key (kbd "C-c C-p") 'mc/mark-previous-like-this)
 ;;; Set key binding for google translate
-(global-set-key (kbd "C-c C-t") 'google-translate-query-translate)
+;; (global-set-key (kbd "C-c C-t") 'google-translate-query-translate)
 ;;; Set default google translate source and target language
-(setq google-translate-default-source-language "en")
-(setq google-translate-default-target-language "zh-CN")
+;; (setq google-translate-default-source-language "en")
+;; (setq google-translate-default-target-language "zh-CN")
 
 ;; The command is used to visit the symbol linked file instead of the symbol file
 (defun visit-target-instead ()
@@ -57,37 +57,6 @@
         (mark-sexp)
         (kill-region (region-beginning) (region-end))))
      (t (delete-backward-char 1)))))
-
-;;; open external app on dired
-(defun open-in-external-app ()
-  "Open the current file or dired marked files in external app.
-The app is chosen from your OS's preference.
-URL `http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html'
-Version 2016-10-15"
-  (interactive)
-  (let* (
-         ($file-list
-          (if (string-equal major-mode "dired-mode")
-              (dired-get-marked-files)
-            (list (buffer-file-name))))
-         ($do-it-p (if (<= (length $file-list) 5)
-                       t
-                     (y-or-n-p "Open more than 5 files? "))))
-    (when $do-it-p
-      (cond
-       ((string-equal system-type "windows-nt")
-        (mapc
-         (lambda ($fpath)
-           (w32-shell-execute "open" (replace-regexp-in-string "/" "\\" $fpath t t))) $file-list))
-       ((string-equal system-type "darwin")
-        (mapc
-         (lambda ($fpath)
-           (shell-command
-            (concat "open " (shell-quote-argument $fpath))))  $file-list))
-       ((string-equal system-type "gnu/linux")
-        (mapc
-         (lambda ($fpath) (let ((process-connection-type nil))
-                       (start-process "" nil "xdg-open" $fpath))) $file-list))))))
 
 ;;; paste or paste previouse
 ;; (defun past-or-past-previouse ()
@@ -234,48 +203,32 @@ Version 2016-10-15"
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (setq web-mode-markup-indent-offset 2)
 
-;;; my custom kbd
+;;; MY CUSTOM KEYS START
 (require 'my-keys-minor-mode)
+;;; MY CUSTOM KEYS END
 
-;;; my scheme config, paredit
+;;; SCHEME CONFIG START
 (autoload 'paredit-mode "paredit"
   "Minor mode for pseudo-structurally editing Lisp code."
   t)
 (require 'scheme-init)
+;;; SCHEME CONFIG END
 
-
-;;; web mode setup
+;;; WEB MODE START
 (require 'web-mode)
-
 (require 'coffee-mode)
+;;; WEB MODE END
 
-;;; c setup
-;;; irony complete function
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'c-mode-hook 'irony-mode)
-(add-hook 'objc-mode-hook 'irony-mode)
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+;;; C CONFIG
+(require 'my-c-config)
 
-;;; yasnippet
-(require 'yasnippet)
-(yas-global-mode 1)
+;;; dired CONFIG
+(require 'my-dired-config)
 
-;;; rtags
-(require 'rtags)
-(setq rtags-path "/home/wpzero/workspace/c_workspace/rtags/bin")
-(setq rtags-autostart-diagnostics t)
-(setq rtags-display-result-backend 'ivy)
-
-;;; Rtags kbd config
-(define-key c++-mode-map (kbd "M-.") 'rtags-find-symbol-at-point)
-(define-key c++-mode-map (kbd "M-,") 'rtags-location-stack-back)
-(define-key c++-mode-map (kbd "C-.") 'rtags-find-references)
-
-(define-key c-mode-map (kbd "M-.") 'rtags-find-symbol-at-point)
-(define-key c-mode-map (kbd "M-,") 'rtags-location-stack-back)
-(define-key c-mode-map (kbd "C-.") 'rtags-find-references)
-
-;;; Add etags table, auto add
-(require 'etags-table)
+;;; Multi-term START
+(require 'multi-term)
+(setq multi-term-program "/bin/zsh")
+(global-set-key (kbd "C-c C-t") 'multi-term)
+;;; Multi-term END
 
 (provide 'custom-init)
---
+++ custom-lisp/my-c-config.el
@@ -0,0 +1,31 @@
+;;; C CONFIG START
+;;; irony complete function
+;;; need to use compile_commands.json to setup cdb
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+;;; yasnippet
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;;; rtags
+(require 'rtags)
+(setq rtags-path "/home/wpzero/workspace/c_workspace/rtags/bin")
+(setq rtags-autostart-diagnostics t)
+(setq rtags-display-result-backend 'ivy)
+
+(define-key c++-mode-map (kbd "M-.") 'rtags-find-symbol-at-point)
+(define-key c++-mode-map (kbd "M-,") 'rtags-location-stack-back)
+(define-key c++-mode-map (kbd "C-.") 'rtags-find-references)
+
+(define-key c-mode-map (kbd "M-.") 'rtags-find-symbol-at-point)
+(define-key c-mode-map (kbd "M-,") 'rtags-location-stack-back)
+(define-key c-mode-map (kbd "C-.") 'rtags-find-references)
+
+;;; add etags table, auto add
+(require 'etags-table)
+;;; C CONFIG END
+
+(provide 'my-c-config)
