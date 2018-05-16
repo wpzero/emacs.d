b0025d9712cc6f8fc5481d28b2fbec29cf514bb2
wpzero <wpcreep@gmail.com>
2018-05-16 16:14:49 +0800

    Modify-multi-term-mode-key-binding-add-term-paste-term-line-mode-term-char-mode

--- custom-lisp/my-multi-term.el
+++ custom-lisp/my-multi-term.el
@@ -6,4 +6,15 @@
 (global-set-key (kbd "C-c C-p") 'multi-term-next)
 ;;; Multi-term END
 
+;;; line mode use term-raw-map
+(define-key term-raw-map (kbd "C-y") 'term-paste)
+;;; char mode use term-mode-map
+(define-key term-mode-map (kbd "C-y") 'term-paste)
+
+(define-key term-mode-map (kbd "C-c C-j") 'term-line-mode)
+(define-key term-raw-map (kbd "C-c C-j") 'term-line-mode)
+
+(define-key term-mode-map (kbd "C-c C-k") 'term-char-mode)
+(define-key term-raw-map (kbd "C-c C-k") 'term-char-mode)
+
 (provide 'my-multi-term)
