f8908b83109bd792d1cc46250c54852400266610
wpzero <wpcreep@gmail.com>
2026-04-04 22:30:25 +0800

    Add TypeScript Eglot keybindings

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -7,6 +7,12 @@
 (when (maybe-require-package 'eglot)
   (maybe-require-package 'consult-eglot)
 
+  (defun sanityinc/setup-typescript-keys ()
+    "Set buffer-local keybindings for TypeScript navigation and refactors."
+    (local-set-key (kbd "C-c . f") #'xref-find-references)
+    (local-set-key (kbd "C-c . r") #'eglot-rename)
+    (local-set-key (kbd "C-c . a") #'eglot-code-actions))
+
   (defun sanityinc/enable-eglot-for-typescript ()
     "Enable Eglot in TypeScript buffers when the language server is available."
     (when (executable-find "typescript-language-server")
@@ -20,6 +26,7 @@
   (dolist (hook '(typescript-mode-hook
                   typescript-ts-mode-hook
                   tsx-ts-mode-hook))
+    (add-hook hook 'sanityinc/setup-typescript-keys)
     (add-hook hook 'sanityinc/enable-eglot-for-typescript)))
 
 
